4e6149430f7db6978df52f0577c68bd1b60a39ec
Ranjan Maiti <ranjanmaiti.tech@gmail.com>
2026-01-15 10:36:54 +0530

    DOM Day 13

---
+++ JavaScript Full Course/Day13_DOM.js
@@ -0,0 +1,33 @@
+//const temp = window.document.getElementById("first")
+//Create full html file into objects so that js can handle
+//temp.textcontent = "Ranjan";  //change text html
+
+
+//const temp = window.document.getElementById("second")
+// temp.innerHTML       //all content inside selected tag
+// 'It is comming on <span style="display: none;">18 October</span>'
+// temp.innerText       //
+// 'It is comming on'
+// temp.textContent     //ignore tag and all text content
+// 'It is comming on 18 October'
+
+
+//const list = document.getElementsByTagName("li")
+//return a type of array
+
+//to convert fully array use spread operator
+//const arr = [...list];
+
+
+//const classbase = document.getElementsByClassName("third")
+//its also a type of array
+// classbase[0]
+
+//also can give id 
+//classbase[0].id = "rohan"
+
+
+//document.quarySelector("h1 or #id or .class")
+//quary selector only select first if multiple class
+
+//for all use quarySelectorAll
