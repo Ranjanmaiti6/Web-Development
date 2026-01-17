d4c92869172286d6d595ba996b23fbb0efa3876c
Ranjan Maiti <ranjanmaiti.tech@gmail.com>
2026-01-17 22:03:45 +0530

    Day 17 Intro API

---
+++ JavaScript Full Course/Day17/day17.js
@@ -0,0 +1,16 @@
+console.log("HEllo Ji");
+setTimeout(() => {
+    console.log("Settime Out 1");
+}, 5000);
+setTimeout(() =>{
+    console.log("Sesion time out 2");
+}, 6000);
+console.log("Hello again");
+
+
+// API is just a function call
+
+//  JS  is a single threaded language run line by line 
+//but dont wait for anyone like if there is any saaettime function dont wait javascript it transfer to web API to wait by timer while not get ans when ans got then check if stack is empty then push to result
+
+//callbackques and microbackques there are two api type micro preference is high in micro there story like apis like github linkden follower api and in callbackques store setitmeout , eventlisterner etc
